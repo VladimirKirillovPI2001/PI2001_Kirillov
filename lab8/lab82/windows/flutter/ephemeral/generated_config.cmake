a6c5bbfb1118a1bd5dd6a43fb3544a07689ad858
VladimirKirillovPI2001 <126083696+VladimirKirillovPI2001@users.noreply.github.com>
2023-05-10 21:37:18 +0300

    8

---
+++ lab8/lab82/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\ChromeDownloads\\flutter_windows_3.7.9-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\PROJ\\lab82" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\ChromeDownloads\\flutter_windows_3.7.9-stable\\flutter"
+  "PROJECT_DIR=D:\\PROJ\\lab82"
+  "FLUTTER_ROOT=D:\\ChromeDownloads\\flutter_windows_3.7.9-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\PROJ\\lab82\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\PROJ\\lab82"
+  "FLUTTER_TARGET=D:\\PROJ\\lab82\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\PROJ\\lab82\\.dart_tool\\package_config.json"
+)
